eb8f04fa724580199aa542da1fba9da74ff8acbf
Dulcie IRVANI <dulcie.irvani@gmail.com>
2023-02-23 18:59:08 +0100

    watch list not finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,29 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #LISTS
+  # A user can see all the lists
+
+  resources :lists, only:[:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
+
+  # get 'lists', to: 'lists#index'
+
+  # # A user can create a new list
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists', to: 'lists#create'
+
+  # # A user can see the details of a given list and its name
+  # get 'lists/:id', to: 'lists#show', as: 'list'
+
+  # #BOOKMARKS
+  # #create
+  # get 'lists/:id/bookmarks/new', to: 'bookmarks#new'
+  # post 'lists/:id/bookmarks', to: 'bookmarks#create'
+  # #delete
+  # delete 'bookmarks/:id', to: 'bookmarks#destroy'
 end
